450d09f63ff540130994df2e7467e9694ee3cece
onemask <onemask@naver.com>
2019-06-19 18:31:30 +0900

    Add Activity Extension

---
+++ app/src/main/java/android/architecture/mvvm/feature/extension/ActivityExt.kt
@@ -0,0 +1,11 @@
+package android.architecture.mvvm.feature.extension
+
+import androidx.annotation.IdRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+fun AppCompatActivity.replaceFragment(@IdRes id: Int, supportFragment: Fragment){
+    supportFragmentManager.beginTransaction()
+        .replace(id,supportFragment)
+        .commitNow()
+}
