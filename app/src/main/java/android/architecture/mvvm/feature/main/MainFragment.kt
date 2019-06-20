26f4f21f5f85cd8d24ba64d726287fcc60e18ccd
onemask <onemask@naver.com>
2019-06-20 17:51:44 +0900

    MainFragment Inject 제거.

--- app/src/main/java/android/architecture/mvvm/feature/main/MainFragment.kt
+++ app/src/main/java/android/architecture/mvvm/feature/main/MainFragment.kt
@@ -1,30 +1,33 @@
 package android.architecture.mvvm.feature.main
 
 
+import android.architecture.mvvm.R
 import android.architecture.mvvm.databinding.FragmentMainBinding
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
-import dagger.android.support.DaggerFragment
-import javax.inject.Inject
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 
 
-class MainFragment : DaggerFragment() {
+class MainFragment : Fragment() {
 
-    @Inject
-    lateinit var binding : FragmentMainBinding
 
-    @Inject
-    lateinit var viewModel: MainFragmentViewModel
+    private lateinit var binding : FragmentMainBinding
+    private lateinit var myView : View
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?)  = binding.root
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View?{
+        binding = DataBindingUtil.inflate(activity!!.layoutInflater, R.layout.fragment_main,null,false)
+        myView = binding.root
+        return myView
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.lifecycleOwner=this
+
+
+       /* binding.lifecycleOwner=this
         binding.viewmodel=viewModel
         viewModel.clickEvent.observe(this, Observer {
             when(it){
@@ -33,7 +36,7 @@ class MainFragment : DaggerFragment() {
                 MainMenu.PREFERENCE_ACTIVITY->Log.d("3","POST_ACTIVITY")
                 MainMenu.PHOTO_ACTIVITY->Log.d("4","PHOTO_ACTIVITY")
             }
-        })
+        })*/
 
     }
 }
