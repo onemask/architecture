8b60e0c41c1466472897b2d3af0b1cab08bb2e29
onemask <onemask@naver.com>
2019-06-20 16:31:05 +0900

    DaggerAppComponent 생성

--- app/src/main/java/android/architecture/mvvm/App.kt
+++ app/src/main/java/android/architecture/mvvm/App.kt
@@ -1,11 +1,12 @@
 package android.architecture.mvvm
 
+import android.architecture.mvvm.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 
 class App : DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
-    TODO()
+        return DaggerAppComponent.builder().create(this)
     }
 
 }
