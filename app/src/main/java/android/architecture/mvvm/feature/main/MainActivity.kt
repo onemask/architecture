15fc081ea2c4979c31c2a809c557de1f1ff487d6
onemask <onemask@naver.com>
2019-06-20 17:25:54 +0900

    MainActivity에서 Inject 제거

--- app/src/main/java/android/architecture/mvvm/feature/main/MainActivity.kt
+++ app/src/main/java/android/architecture/mvvm/feature/main/MainActivity.kt
@@ -4,24 +4,18 @@ import android.architecture.mvvm.R
 import android.architecture.mvvm.databinding.ActivityMainBinding
 import android.architecture.mvvm.feature.extension.replaceFragment
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
 
 class MainActivity : DaggerAppCompatActivity() {
 
-    @Inject
-    lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        //kotlin null 처리로 refactoring
-        /* if (savedInstanceState==null){
-            replaceFragment(R.id.container,MainFragment())
-        }*/
-        savedInstanceState?:run {
-            replaceFragment(R.id.container,MainFragment())
-        }
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
+        replaceFragment(R.id.container, MainFragment())
     }
 
 }
