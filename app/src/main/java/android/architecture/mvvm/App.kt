ade602b1f9f1af899b9965ca3271e78448206054
onemask <onemask@naver.com>
2019-06-27 15:20:09 +0900

    Add : Timber Library

--- app/src/main/java/android/architecture/mvvm/App.kt
+++ app/src/main/java/android/architecture/mvvm/App.kt
@@ -3,8 +3,19 @@ package android.architecture.mvvm
 import android.architecture.mvvm.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
+import timber.log.Timber
 
 class App : DaggerApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+        setupTimber()
+    }
+
+    private fun setupTimber() {
+        Timber.plant(Timber.DebugTree())
+    }
+
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.builder().create(this)
     }
--- app/src/main/java/android/architecture/mvvm/feature/main/MainFragment.kt
+++ app/src/main/java/android/architecture/mvvm/feature/main/MainFragment.kt
@@ -1,42 +1,35 @@
 package android.architecture.mvvm.feature.main
 
 
-import android.architecture.mvvm.R
 import android.architecture.mvvm.databinding.FragmentMainBinding
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 
 
 class MainFragment : Fragment() {
 
 
     private lateinit var binding : FragmentMainBinding
-    private lateinit var myView : View
+    private lateinit var viewModel: MainFragmentViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View?{
-        binding = DataBindingUtil.inflate(activity!!.layoutInflater, R.layout.fragment_main,null,false)
-        myView = binding.root
-        return myView
-    }
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) = binding.root
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
-       /* binding.lifecycleOwner=this
+        binding.lifecycleOwner=this
         binding.viewmodel=viewModel
         viewModel.clickEvent.observe(this, Observer {
             when(it){
-                MainMenu.PERSON_ACTIVITY->Log.d("1","PERSON_ACTIVITY")
+                MainMenu.PERSON_ACTIVITY-> Log.d("1","PERSON_ACTIVITY")
                 MainMenu.POST_ACTIVITY->Log.d("2","POST_ACTIVITY")
                 MainMenu.PREFERENCE_ACTIVITY->Log.d("3","POST_ACTIVITY")
                 MainMenu.PHOTO_ACTIVITY->Log.d("4","PHOTO_ACTIVITY")
             }
-        })*/
+        })
 
     }
 }
